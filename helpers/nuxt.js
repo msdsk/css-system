88d1bc2535048583cbdffb960237792242216939
Michal Sadowski <saski.michal.sadowski@gmail.com>
2020-12-06 11:28:10 +0100

    Add imports

--- helpers/nuxt.js
+++ helpers/nuxt.js
@@ -2,12 +2,14 @@
 Call this module from nuxt.config.js build.extend to be able to override the variables
 */
 
-module.exports = function(loaders, pathToVariables='assets/scss/_variables.scss') {
+const fs = require('fs')
+
+module.exports = function variableOverride(loaders, pathToVariables='assets/scss/_variables.scss') {
   loaders.scss.webpackImporter = true
   loaders.scss.sassOptions = {
     importer(url) {
       if (url === 'variables') {
-        const scss = fs.readFileSync(path.join(__dirname, pathToVariables), 'utf8', console.error)
+        const scss = fs.readFileSync(pathToVariables, 'utf8', console.error)
         return { contents: scss }
       }
       return null
